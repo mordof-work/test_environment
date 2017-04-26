f87e363398497aa25613af46b603b01978174f4c
mordof-work <micheal.winger@gshiftlabs.com>
2017-04-26 16:28:00 -0400

    clean up structure

--- init.rb
+++ init.rb
@@ -17,3 +17,19 @@ ActiveRecord::Base.establish_connection(
 
 require_all 'models/application_record.rb'
 require_all 'models'
+
+# Actual Code for the Test
+
+Agency.delete_all
+Client.delete_all
+Presence.delete_all
+Node.delete_all
+
+agency = Agency.create name: "agency"
+client = Client.create name: "client", agency_id: agency.id
+presence = Presence.create name: "presence", client_id: client.id
+
+binding.pry if ENV['DEBUG'].present?
+
+puts ""
+
