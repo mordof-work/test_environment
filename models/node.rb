087580b455cd14ac46b1484732002eab18206b88
mordof-work <micheal.winger@gshiftlabs.com>
2017-04-26 14:57:51 -0400

    focus on agency - still a problem

--- models/node.rb
+++ models/node.rb
@@ -4,13 +4,11 @@ class Node < ApplicationRecord
 
     ######################
     #
-    #  When these three are commented out, there is no 'stack level too deep'
+    #  When this is commented out, there is no 'stack level too deep'
     #  issue.
     #
     ######################
 
     has_one :agency
-    has_one :client
-    has_one :presence
   end
 end
--- test/test.rb
+++ test/test.rb
@@ -2,8 +2,8 @@ require 'require_all'
 require_all 'init.rb'
 
 agency = Agency.create name: "agency"
-client = Client.create name: "client", agency_id: agency.id
-presence = Presence.create name: "presence", client_id: client.id
+# client = Client.create name: "client", agency_id: agency.id
+# presence = Presence.create name: "presence", client_id: client.id
 
 binding.pry if ENV['DEBUG'].present?
 
