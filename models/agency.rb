b21b46e4d0568370f2bd8a1e00b13a03c4c9545a
mordof-work <micheal.winger@gshiftlabs.com>
2017-04-27 17:18:17 -0400

    found the actual culprit

--- models/agency.rb
+++ models/agency.rb
@@ -19,10 +19,23 @@ class Agency < ApplicationRecord
     can_continue = !new_record?
     yield
     return true unless can_continue
+
+    puts node.present?
+    puts name_changed?
+    puts is_deleted_changed?
+    ##########
+    ## This line is critical!!!! At this point node is not present.
+    ## It looks like ActiveRecord/ActiveModel (not sure which) changed the
+    ## order in which after_create gets called in relation to around_save.
+    #
+    ## I also wonder if this was an intentional change.
+    ##########
     if node.present? && (name_changed? || is_deleted_changed?)
-      node.name = name
-      node.is_deleted = is_deleted
-      node.save
+      # node.name = name
+      # node.is_deleted = is_deleted
+      # Agency.suppress do
+        node.save
+      # end
     end
   end
 end
